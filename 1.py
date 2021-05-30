4468410f85cb270469b8bb142be143c7f7ae32bc
Csabatron99 <55226509+Csabatron99@users.noreply.github.com>
2021-05-30 15:09:10 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,33 @@
+import discord
+import string
+import random
+
+CLIENT = discord.Client()
+
+TOKEN = 'MzIwNjM0MTY2ODcyMTEzMTUz.WTMD1g.qF7yZZY9pEheBbMtOWsbhi_uaG0'
+
+
+@CLIENT.event
+async def on_ready():
+    print('Sikeres bejelentkezés {0.user}!'.format(CLIENT))
+
+imgcode = ['https://imgur.com/t/dank_memes/obglZ5L', 'https://imgur.com/t/dank_memes/b3k4xyd', 'https://imgur.com/t/dank_memes/bmdfSM3','https://imgur.com/t/dank_memes/y2qVnMG','https://imgur.com/t/dank_memes/ri1x40j']
+
+@CLIENT.event
+async def on_message(message):
+    if message.author == CLIENT.user:
+        return
+    if message.content.startswith('!Hi'):
+        await message.channel.send('Halika :3 ')
+    if message.content.startswith('!parancsok'):
+        await message.channel.send('!hi \n'
+                                   '!meme')
+    if message.content.startswith('!meme'):  
+        wrap = discord.Embed(title="", type="rich")
+        img = imgcode[random.randint(0,4)]
+        print(img)
+        wrap.set_image(url=img)
+        await message.channel.send(img)
+        
+        
+CLIENT.run(TOKEN)
